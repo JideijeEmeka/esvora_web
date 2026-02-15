503801bdab6bf0ad8288afec3b4f967a7e999121
Emeka Jideije <jideije.emeka@gmail.com>
2026-02-15 03:25:38 +0100

    add app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -26,12 +26,15 @@ import RentView from './views/rent_view'
 import ShortletView from './views/shortlet_view'
 import LandlordDetailsView from './views/landlord_details_view'
 import PropertyOwnerView from './views/property_owner_view'
+import RequestsView from './views/requests_view'
+import RequestDetailsView from './views/request_details_view'
 import KycView from './views/kyc_view'
 import KycFormView from './views/kyc_form_view'
 import KycSelectIdView from './views/kyc_select_id_view'
 import KycQrcodeView from './views/kyc_qrcode_view'
 import KycSubmitSuccessView from './views/kyc_submit_success_view'
 import AddPropertyView from './views/add_property_view'
+import EditListingView from './views/edit_listing_view'
 import AddPropertyBasicInfoFormView from './views/add_property_basic_info_form_view'
 import AddPropertyFeaturesFormView from './views/add_property_features_form_view'
 import AddPropertyPriceFormView from './views/add_property_price_form_view'
@@ -40,6 +43,7 @@ import AddPropertyBedroomsView from './views/add_property_bedrooms_view'
 import AddPropertyImagesView from './views/add_property_images_view'
 import AddPropertySummaryInfoView from './views/add_property_summary_info_view'
 import AddPropertyAgreementAndPolicyView from './views/add_property_agreement_and_policy_view'
+import AddForSaleAgreementAndPolicyView from './views/add_for_sale_agreement_and_policy_view'
 import AddPropertySuccessView from './views/add_property_success_view'
 import AddShortletView from './views/add_shortlet_view'
 import AddShortletBasicInfoFormView from './views/add_shortlet_basic_info_form_view'
@@ -49,6 +53,14 @@ import AddShortletHouseRegulationsView from './views/add_shortlet_house_regulati
 import AddShortletBedroomsView from './views/add_shortlet_bedrooms_view'
 import AddShortletImagesView from './views/add_shortlet_images_view'
 import AddShortletSummaryInfoView from './views/add_shortlet_summary_info_view'
+import AddForSaleView from './views/add_for_sale_view'
+import AddForSaleBasicInfoFormView from './views/add_for_sale_basic_info_form_view'
+import AddForSaleFeaturesView from './views/add_for_sale_features_view'
+import AddForSalePriceView from './views/add_for_sale_price_view'
+import AddForSaleDocumentsView from './views/add_for_sale_documents_view'
+import AddForSaleBedroomsView from './views/add_for_sale_bedrooms_view'
+import AddForSaleImagesView from './views/add_for_sale_images_view'
+import AddForSaleSummaryInfoView from './views/add_for_sale_summary_info_view'
 
 const App = () => {
   return (
@@ -78,7 +90,10 @@ const App = () => {
         <Route path="/reviews" element={<ReviewsView />} />
         <Route path="/my-properties" element={<MyPropertiesView />} />
         <Route path="/" element={<PropertyOwnerView />} />
+        <Route path="/property-owner/requests" element={<RequestsView />} />
+        <Route path="/property-owner/requests/:id" element={<RequestDetailsView />} />
         <Route path="/property-owner/listings" element={<AddPropertyView />} />
+        <Route path="/property-owner/listings/edit/:id" element={<EditListingView />} />
         <Route path="/property-owner/add-property/basic-info" element={<AddPropertyBasicInfoFormView />} />
         <Route path="/property-owner/add-property/features" element={<AddPropertyFeaturesFormView />} />
         <Route path="/property-owner/add-property/price" element={<AddPropertyPriceFormView />} />
@@ -87,8 +102,17 @@ const App = () => {
         <Route path="/property-owner/add-property/images" element={<AddPropertyImagesView />} />
         <Route path="/property-owner/add-property/summary" element={<AddPropertySummaryInfoView />} />
         <Route path="/property-owner/add-property/agreement" element={<AddPropertyAgreementAndPolicyView />} />
+        <Route path="/property-owner/add-sale/agreement" element={<AddForSaleAgreementAndPolicyView />} />
         <Route path="/property-owner/add-property/success" element={<AddPropertySuccessView />} />
         <Route path="/property-owner/shortlet" element={<AddShortletView />} />
+        <Route path="/property-owner/sale" element={<AddForSaleView />} />
+        <Route path="/property-owner/add-sale/basic-info" element={<AddForSaleBasicInfoFormView />} />
+        <Route path="/property-owner/add-sale/features" element={<AddForSaleFeaturesView />} />
+        <Route path="/property-owner/add-sale/price" element={<AddForSalePriceView />} />
+        <Route path="/property-owner/add-sale/documents" element={<AddForSaleDocumentsView />} />
+        <Route path="/property-owner/add-sale/bedrooms" element={<AddForSaleBedroomsView />} />
+        <Route path="/property-owner/add-sale/images" element={<AddForSaleImagesView />} />
+        <Route path="/property-owner/add-sale/summary" element={<AddForSaleSummaryInfoView />} />
         <Route path="/property-owner/add-shortlet/basic-info" element={<AddShortletBasicInfoFormView />} />
         <Route path="/property-owner/add-shortlet/features" element={<AddShortletFeaturesView />} />
         <Route path="/property-owner/add-shortlet/price" element={<AddShortletPriceView />} />
