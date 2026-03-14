6b9c44533ae5fd988fd9efba457d8ebe2959e9fa
Emeka Jideije <jideije.emeka@gmail.com>
2026-03-14 06:53:33 +0100

    send update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
+// Env is determined by Vite mode: `vite` loads .env.development (dev), `vite build` loads .env.production (prod).
+// See src/lib/constants.jsx for Env, baseUrl, kDebugMode.
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { store } from './redux/store'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
 )
