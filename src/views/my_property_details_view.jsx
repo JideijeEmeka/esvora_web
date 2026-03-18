20fc20e66fdfb4190b46493c6a8029f773906b71
Emeka Jideije <jideije.emeka@gmail.com>
2026-03-18 13:59:03 +0100

    send an update

---
+++ src/views/my_property_details_view.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import PropertyDetailsView from './property_details_view'
+
+const MyPropertyDetailsView = () => <PropertyDetailsView onlyRateProperty />
+
+export default MyPropertyDetailsView
